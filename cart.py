5ff29792830fef813e834eeae8e579c006d6632b
LeontescuRadu <leontescuradu24@gmail.com>
2025-03-14 23:58:57 +0200

    Adaugat clasa Cart si functionalitatea aferenta in main.py

---
+++ cart.py
@@ -0,0 +1,15 @@
+# cart.py
+
+class Cart:
+    def __init__(self):
+        self.cart_items = []
+
+    def add_to_cart(self, product):
+        self.cart_items.append(product)
+
+    def calculate_total(self):
+        return sum(item.price * item.quantity for item in self.cart_items)
+
+    def display_cart(self):
+        for item in self.cart_items:
+            print(f"{item.name} - {item.price} x {item.quantity}")
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 # Crearea instanței ProductManager
 manager = ProductManager()
@@ -15,3 +16,14 @@ manager.display_products()
 
 # Afișarea valorii totale
 manager.display_total_value()
+
+# Crearea instanței Cart
+cart = Cart()
+
+# Adăugarea produselor în coș
+cart.add_to_cart(Product("Laptop", 3500, 1))
+cart.add_to_cart(Product("Telefon", 2000, 2))
+
+# Afișarea conținutului coșului și valoarea totală
+cart.display_cart()
+print(f"Total Cart Value: {cart.calculate_total()}")
