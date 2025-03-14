12be3810fd3e610c9bd2e2da7f15caef392d96c9
LeontescuRadu <leontescuradu24@gmail.com>
2025-03-14 23:51:46 +0200

    Adaugat functionalitatea de stergere a produselor in ProductManager

--- product_manager.py
+++ product_manager.py
@@ -16,3 +16,6 @@ class ProductManager:
     def display_total_value(self):
         total = sum(product.price * product.quantity for product in self.products)
         print(f"Total Inventory Value: {total}")
+
+    def remove_product(self, product_name):
+        self.products = [product for product in self.products if product.name != product_name]
